cc4fad3c657bf75918ec8ffc96416ce2d2946540
Elias Waagaard <elias.waagaard@ipp.mpg.de>
2026-09-07 11:44:40 +0200

    Updated source equation terms to include vpar only, like model 600

--- models/model183/mod_equations.f90
+++ models/model183/mod_equations.f90
@@ -571,15 +571,12 @@ module mod_equations
       rhs_semianalytic(var_vpar) = rhs_semianalytic(var_vpar) + tstep*v*aux_mom_par0 ! momentum source to momentum equation
 
       rhs_semianalytic(var_T) = rhs_semianalytic(var_T)                               & 
-                              - tstep*(gamma-1.d0)*v*aux_mom_par0*vpar0               ! momentum source to pressure equation
-      #if INCLUDE_ADDITIONAL_TERMS
-        rhs_semianalytic(var_T) = rhs_semianalytic(var_T) + tstep*(gamma-1.d0)*0.5d0*v*aux_rho0*v2          ! v2 alone is the total vsquared needed here
-      #else
-        rhs_semianalytic(var_T) = rhs_semianalytic(var_T) + tstep*(gamma-1.d0)*0.5d0*v*aux_rho0*(v2 + vpar2) ! mass source to pressure equation. disjoint pieces, sum needed
-      #endif
+                              - tstep*(gamma-1.d0)*v*aux_mom_par0*vpar0               & ! momentum source to pressure equation
+                              + tstep*(gamma-1.d0)*0.5d0*v*aux_rho0*vpar0*vpar0*B2      ! parallel-only, similar to aux_par0 and  
+                                                                                        ! matches model 600 convention
 
       ! LHS entries due to vpar parametric dependence: theta * d(RHS(i))/dx_j. Partial derivatives already computed above
-      ! note: vpar2_Psi/v2_Phi/vpar2_vpar already computed and automatically equal, so no branching for these amat entries below 
+      ! #if INCLUDE_ADDITIONAL_TERMS branch (seemingly not used), vpar2 is aliased to the full v2 also with ExB term
       amat_semianalytic(var_T,var_vpar) = amat_semianalytic(var_T,var_vpar)                      &
                                         + tstep*theta*(gamma-1.d0)*v*aux_mom_par0*vpar           &
                                         - tstep*theta*(gamma-1.d0)*0.5d0*v*aux_rho0*vpar2_vpar
@@ -587,8 +584,8 @@ module mod_equations
       amat_semianalytic(var_T,var_Psi) = amat_semianalytic(var_T,var_Psi)                        &
                                        - tstep*theta*(gamma-1.d0)*0.5d0*v*aux_rho0*vpar2_Psi
 
-      amat_semianalytic(var_T,var_Phi) = amat_semianalytic(var_T,var_Phi)                        &
-                                       - tstep*theta*(gamma-1.d0)*0.5d0*v*aux_rho0*v2_Phi
+      ! amat_semianalytic(var_T,var_Phi) = amat_semianalytic(var_T,var_Phi)                        &
+      !                                 - tstep*theta*(gamma-1.d0)*0.5d0*v*aux_rho0*v2_Phi  ! REMOVE - no Phi-dependence once v2 (ExB term) is dropped
     endif
 
     ! Expansion of differential operators
