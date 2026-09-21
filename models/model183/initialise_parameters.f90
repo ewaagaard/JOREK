f8846cd84bdd40eb49ded9a556cd84b024c5b2c9
Elias Waagaard <elias.waagaard@ipp.mpg.de>
2026-09-21 13:22:00 +0200

    Added freeze_Psi in initialise_parameters and broadcast_phys

--- models/model183/initialise_parameters.f90
+++ models/model183/initialise_parameters.f90
@@ -43,6 +43,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 n_pfc, n_tor_fft_thresh, manipulate_psi_map,        &
                 Rmin_pfc, Rmax_pfc, Zmin_pfc, Zmax_pfc, current_pfc,&
                 tokamak_device, gvec_grid_import,bloating_factor,   &
+                freeze_psi_dynamics,                                &
                 F0, gamma_sheath, density_reflection,               &
                 zjz_0, zjz_1, zj_coef,                              &
                 rho_0, rho_1, rho_coef,                             &
